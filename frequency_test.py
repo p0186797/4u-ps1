72e452b428fca64205960021b010683d6bec3bb0
p0186797 <76880486+p0186797@users.noreply.github.com>
2022-03-02 08:39:51 -0500

    Add files via upload

---
+++ frequency_test.py
@@ -0,0 +1,7 @@
+from frequency import *
+
+print(most_frequent(["please"]))
+print(most_frequent(["meadowvale"]))
+print(most_frequent(["apple", "shape"]))
+print(most_frequent(["cease","snack", "cash", "cream", "care", "peace", "special"]))
+print(most_frequent(["caste", "you" "blue", "eye", "owl", "and", "a", "your", "ouch", "bat"]))
