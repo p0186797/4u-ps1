b756c072232102f59c35a5e3e5fe8b1c28f772f9
p0186797 <76880486+p0186797@users.noreply.github.com>
2022-03-02 07:30:07 -0500

    Add files via upload

---
+++ compatability_test.py
@@ -0,0 +1,7 @@
+from zodiac import *
+
+print(get_compatibility('Aries', 'Taurus'))
+print(get_compatibility('Libra', 'Virgo'))
+print(get_compatibility('Gemini', 'Aquarius'))
+print(get_compatibility('Pisces', 'Cancer'))
+print(get_compatibility('Cancer', 'Cancer'))
