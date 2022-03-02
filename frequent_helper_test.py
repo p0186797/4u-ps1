46c5eb2d352be89c384fe590dc4646c6df64ecd9
p0186797 <76880486+p0186797@users.noreply.github.com>
2022-03-02 08:39:33 -0500

    Add files via upload

---
+++ frequent_helper_test.py
@@ -0,0 +1,7 @@
+from frequency import *
+
+print(most_frequent_helper("a", ["meadowvale"]))
+print(most_frequent_helper("z", ["meadowvale"]))
+print(most_frequent_helper("b", ["apple", "shape"]))
+print(most_frequent_helper("c", ["caste", "a", "you" "slash", "elephant"]))
+print(most_frequent_helper("e", ["apple", "at", "cream", "car", "pea", "special"]))
