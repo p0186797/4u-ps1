6bd4001cdf6172e69353303aa102581632e4b22f
p0186797 <76880486+p0186797@users.noreply.github.com>
2022-03-02 07:29:52 -0500

    Add files via upload

---
+++ zodiac_test.py
@@ -0,0 +1,7 @@
+from zodiac import *
+
+print(get_zodiac("12-11-2005"))
+print(get_zodiac("01-04-2011"))
+print(get_zodiac("27-07-1999"))
+print(get_zodiac("22-12-1990"))
+print(get_zodiac("23-08-2000"))
