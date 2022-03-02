8f2d6ff08b2d04daecbe2698f585c32ad0da4be9
p0186797 <76880486+p0186797@users.noreply.github.com>
2022-03-02 08:39:09 -0500

    Add files via upload

---
+++ diagonals_test.py
@@ -0,0 +1,9 @@
+from diagonals_solutions import *
+
+print(sorted(get_diagonals([(3, -4), (2, 0), (5, 1)])))
+print(sorted(get_diagonals([(3, 1), (4, -3), (2, 1), (-5, 2)])))
+print(sorted(get_diagonals([(2, -1), (5, -3), (4, 3), (5, -2)])))
+print(sorted(get_diagonals([(8, 4), (3, 4), (4, 5), (7, 3), (-3, 3)])))
+print(sorted(get_diagonals([(-3,5), (-4, 3), (18, 2), (10, 16), (13, -9), (15, 1), (18, -11)])))
+
+
